562df94005a091cc1ed05a7f2ebd3dca5f9f413d
PensCup87 <bdt9123@gmail.com>
2017-10-11 23:54:04 -0400

    Thought I saved everyting

--- Week3_Day3_Inheritance_HomeWork/FlyingDino.cs
+++ Week3_Day3_Inheritance_HomeWork/FlyingDino.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Week3_Day3_Inheritance_HomeWork
 {
-    class FlyingDino
+    class FlyingDino : Dinosaurs
     {
+        public int numberOfWings;
+        public string color;
+    }
+
+    public int NumberOfWings
+    {
+        get { return this.NumberOfWings; } 
+    }
+
+    public string Color
+    {
+        get { return this.Color; }
+    }
+
+    public FlyingDino(int numberOfWings, string color, string name, string moveStyle, string eats)
+    {
+        this.NumberOfWings = numberOfWings;
+        this.Color = color;
+        this.name = name;
+        this.moveStyle = moveStyle;
+        this.eats = eats;
+    }
+
+    public override void Running()
+    {
+
     }
 }
