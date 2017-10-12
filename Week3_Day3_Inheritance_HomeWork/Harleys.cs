0d04f5e3fc6d9c0d1a7ad437823ec2e1be80381b
PensCup87 <bdt9123@gmail.com>
2017-10-11 20:24:08 -0400

    Created 4 fields, 4 properties, and deafult constructor

---
+++ Week3_Day3_Inheritance_HomeWork/Harleys.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Day3_Inheritance_HomeWork
+{
+    class Harleys
+    {
+        //fields
+        protected int engineSize;
+        protected double fuelTankCapacity;
+        protected string rearTireWidth;
+        protected int weight;
+
+        //Properties (Instance Variables)
+
+        protected int EngineSize
+        {
+            get { return this.engineSize; }
+
+        }
+        protected double FuelTankCapacity
+        {
+            get { return this.fuelTankCapacity; }
+            set { this.fuelTankCapacity = value; }
+        }
+        protected string RearTireWidth
+        {
+            get { return this.rearTireWidth; }
+            set { this.rearTireWidth = value; }
+        }
+        protected int Weight
+        {
+            get { return this.weight; }
+            set { this.weight = value; }
+        }
+
+        //Constructor(s)
+        public Harleys()
+        {
+            //default constructor
+        }
+
+    }
+}
